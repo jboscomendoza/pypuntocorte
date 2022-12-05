f3ef21b5c4ad57ff87769b9288d43661173e521f
Juan Bosco Mendoza Vega <jboscomendoza@gmail.com>
2022-12-05 11:52:19 -0600

    Items dummy

--- irt_dummy_source.R
+++ irt_dummy_source.R
@@ -8,8 +8,15 @@ examen <- data.melab$data
 modelo   <- tam.mml(examen)
 personas <- tam.wle(modelo)
 
+items = modelo$xsi
+items = items * 10
+items$b = items$b + 50
+names(items) = c("b", "error")
+items$id = row.names(modelo$xsi)
+
 theta  = (personas$theta * 10) + 50
 error  = sqrt((personas$error * 10))
 scores = data.frame(theta, error) 
 
 write.csv(scores, file = "irt_dummy.csv", row.names = FALSE)
+write.csv(items, file = "irt_items.csv", row.names = FALSE)
