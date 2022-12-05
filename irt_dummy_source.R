98c5db4d4c60681fcc618f34c9757157edf57005
Juan Bosco Mendoza Vega <jboscomendoza@gmail.com>
2022-12-05 12:50:22 -0600

    Corregido error en reescalamiento

--- irt_dummy_source.R
+++ irt_dummy_source.R
@@ -1,22 +1,31 @@
 # install.packages("TAM")
 library(TAM)
 
-data("data.melab")
 
+# Datos incluidos en TAM
+data("data.melab")
 examen <- data.melab$data
 
+
+# Parametros de los items
 modelo   <- tam.mml(examen)
+
+items <- modelo$xsi
+items <- items * 10
+names(items) <- c("b", "error")
+
+items$b  <- items$b + 50
+items$id <- row.names(modelo$xsi)
+
+
+# Parametros de las personas
 personas <- tam.wle(modelo)
 
-items = modelo$xsi
-items = items * 10
-items$b = items$b + 50
-names(items) = c("b", "error")
-items$id = row.names(modelo$xsi)
+theta  <- (personas$theta * 10) + 50
+error  <- sqrt((personas$error * 10))
+scores <- data.frame(theta, error)
 
-theta  = (personas$theta * 10) + 50
-error  = sqrt((personas$error * 10))
-scores = data.frame(theta, error) 
 
+# Exportar a csv
 write.csv(scores, file = "irt_dummy.csv", row.names = FALSE)
 write.csv(items, file = "irt_items.csv", row.names = FALSE)
